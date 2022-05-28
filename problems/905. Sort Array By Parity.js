b6448ae4cdb6f595f5a4b9095fddd55e301c52cb
Max Chou <max9159@gmail.com>
2022-05-28 14:56:03 +0800

    905. Sort Array By Parity_2

--- problems/905. Sort Array By Parity.js
+++ problems/905. Sort Array By Parity.js
@@ -5,10 +5,9 @@
  var arrayPairSum = function (nums) {
   const sortedNums = nums.sort((a, b) => b - a);
   let sum = 0;
-  for (let i = 0; i <= sortedNums.length - 1; i = i + 2) {
-    const currNum = sortedNums[i];
+  for (let i = 0; i <= sortedNums.length - 1; i += 2) {
     const nextNum = sortedNums[i + 1];
-    sum = sum + Math.min(currNum, nextNum);
+    sum += nextNum;
   }
   return sum;
 };
