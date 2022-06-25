be36414af5d1bef5373da123a906cf51bd693967
Max Chou <max9159@gmail.com>
2022-06-25 16:31:53 +0800

    47. Permutations II - Enhancement

--- problems/BackTrack/47. Permutations II.js
+++ problems/BackTrack/47. Permutations II.js
@@ -6,24 +6,30 @@ var permuteUnique = function (nums) {
 
   function backTrack(nums, tracks, res) {
     if (tracks.length === nums.length) {
-      res.push(tracks.slice());
+      const val = JSON.stringify(tracks);
+      if(!memo[val]){
+        res.push(tracks.slice());
+        memo[val] = val;
+      }
+
       return;
     }
 
     for (let i = 0; i < nums.length; i++) {
-      if (memo[i]) continue;
-      if (i > 0 && nums[i] === nums[i - 1] && !memo[i - 1]) continue;
+      if (used[i]) continue;
+      if (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) continue;
 
       tracks.push(nums[i]);
-      memo[i] = true;
+      used[i] = true;
       backTrack(nums, tracks, res);
       tracks.pop();
-      memo[i] = false;
+      used[i] = false;
     }
 
     return res;
   }
-  let memo = new Array(nums.length).fill(false);
+  let used = new Array(nums.length).fill(false);
+  let memo = {};
   // sorting for the same ele together
   return backTrack(nums.sort((a, b) => a - b), [], []);
 };
