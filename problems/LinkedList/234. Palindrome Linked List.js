e52d4dcc8c4c9e810dada7f7a4eba9cc8bbefd82
Max Chou <max9159@gmail.com>
2022-07-12 23:16:04 +0800

    234. Palindrome Linked List - find mid node

--- problems/LinkedList/234. Palindrome Linked List.js
+++ problems/LinkedList/234. Palindrome Linked List.js
@@ -27,3 +27,40 @@ var isPalindrome = function (head) {
   let left = head; // is left pointer
   return traverse(head);
 };
+
+// timecomplexity Big(N) ,space complexity Big(1) 
+// Solution 2 : Get middle of the node
+// boolean isPalindrome(ListNode head) {
+//   ListNode slow, fast;
+//   slow = fast = head;
+//   // ============ Hints : fast pointer always move 2 nodes more than slow pointer ===========
+//   while (fast != null && fast.next != null) {
+//       slow = slow.next;
+//       fast = fast.next.next;
+//   }
+  
+//   if (fast != null)
+//       slow = slow.next;
+  
+//   ListNode left = head;
+//   ListNode right = reverse(slow);
+//   while (right != null) {
+//       if (left.val != right.val)
+//           return false;
+//       left = left.next;
+//       right = right.next;
+//   }
+  
+//   return true;
+// }
+
+// ListNode reverse(ListNode head) {
+//   ListNode pre = null, cur = head;
+//   while (cur != null) {
+//       ListNode next = cur.next;
+//       cur.next = pre;
+//       pre = cur;
+//       cur = next;
+//   }
+//   return pre;
+// }
