a2258217e6d8902f348451610d859b4317a25cd2
Max Chou <max9159@gmail.com>
2022-07-25 23:03:28 +0800

    1071. Greatest Common Divisor of Strings (gcd) .js

---
+++ problems/Others/1071. Greatest Common Divisor of Strings (gcd) .js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    const [smallerString, largerString] = [str1, str2].sort((a,b) => a.length - b.length);
+    for (let i = smallerString.length; i > 0; i--) {
+        const testString = smallerString.slice(0, i);
+        const correctSmaller = !smallerString.split(testString).join('').length;
+        const correctLarger = !largerString.split(testString).join('').length;
+        if (correctSmaller && correctLarger) return testString;
+    }
+    return '';
+};
+
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+ const gcdOfStrings = (str1, str2) => {
+  if (str1 + str2 !== str2 + str1) return '';
+  const gcd = (a, b) => (0 === b ? a : gcd(b, a % b));
+  return str1.substring(0, gcd(str1.length, str2.length));
+};
+
+// 最大公因數 greatest common divisor，gcd
+// Euclidean algorithm 輾轉相除法，又稱歐幾里得算法
+// Example:
+// 252 % 105 = 42
+// 105 % 42 = 21
+// 42 % 21 = 0
