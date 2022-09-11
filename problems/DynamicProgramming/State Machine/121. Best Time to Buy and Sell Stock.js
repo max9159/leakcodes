e47de677a39b41e40ec72534c4ddb98755cf5945
Max Chou <max9159@gmail.com>
2022-09-11 17:53:02 +0800

    121. Best Time to Buy and Sell Stock : Solution 2

--- problems/DynamicProgramming/State Machine/121. Best Time to Buy and Sell Stock.js
+++ problems/DynamicProgramming/State Machine/121. Best Time to Buy and Sell Stock.js
@@ -18,4 +18,31 @@ var maxProfit = function (prices) {
   }
   return dp[n - 1][0];
 };
+
+// =================================== Solution 2 ==============================
+// Space Complexity : O(1)
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let n = prices.length;
+  let maxProfit = 0, latestMinimumBuy = Number.NEGATIVE_INFINITY;
+  for (let i = 0; i < n; i++) {
+    // confirm max pofit for rest / sell.
+    maxProfit = Math.max(maxProfit, latestMinimumBuy + prices[i]);
+    // get minium buy price
+    latestMinimumBuy = Math.max(latestMinimumBuy, -prices[i])
+  }
+  return maxProfit;
+};
+// const input = [7, 1, 5, 3, 6, 4];
+// const expectedResult = 5;
+// result history
+// 0, -7 
+// 0, -1 dp_i_0 = Math.max(0, -7 + 1);, dp_i_1 = Math.max(0, -1)
+// 4, -1
+// 4, -1
+// 5, -1
+// 5, -1
 module.exports = maxProfit;
